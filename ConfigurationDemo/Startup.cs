31515e943fc4550539dde1178e0d3a520d7e8c0e
Zafer Khourdaji <khourdaji@gmail.com>
2019-02-13 15:49:32 -0500

    Add project files.

---
+++ ConfigurationDemo/Startup.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace ConfigurationDemo
+{
+    public class Startup
+    {
+        private readonly IHostingEnvironment _env;
+        private readonly IConfiguration _config;
+        private readonly ILoggerFactory _loggerFactory;
+
+        /*
+         * The host provides services that are available to the Startup class constructor. 
+         * The app adds additional services via ConfigureServices. 
+         * Both the host and app services are then available in Configure and throughout the app.
+         */
+        public Startup(IHostingEnvironment env, IConfiguration config, ILoggerFactory loggerFactory)
+        {
+            _env = env;
+            _config = config;
+            _loggerFactory = loggerFactory;
+        }
+
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            ILogger logger = _loggerFactory.CreateLogger<Startup>();
+
+            if (_env.IsDevelopment())
+            {
+                logger.LogInformation("Development environment");
+            }
+            else
+            {
+                logger.LogInformation($"Environment: {_env.EnvironmentName}");
+            }
+
+            // Configuration is available during startup.
+            // Examples:
+            //   _config["key"]
+            //   _config["subsection:suboption1"]
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+            app.UseMvc();
+        }
+    }
+}
